aa6b93c0a1aa165b0935f87802398bebd11977dd
Sam <Sam@Sam-PC>
2019-11-06 01:12:35 +0800

    Update testing program

--- src/main/java/org/apache/pdfbox/pdmodel/DocumentWriter.java
+++ src/main/java/org/apache/pdfbox/pdmodel/DocumentWriter.java
@@ -3,6 +3,9 @@ package org.apache.pdfbox.pdmodel;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -26,16 +29,16 @@ import net.miginfocom.swing.MigLayout;
 
 public class DocumentWriter implements ActionListener {
 
-	private JTextComponent tfText, pfOwner, pfUser = null;
+	private JTextComponent tfText, pfOwner, pfUser, tfFile = null;
 
-	private AbstractButton btnExecute = null;
+	private AbstractButton btnExecute, btnCopy = null;
 
 	private DocumentWriter() {
 	}
 
 	private void init(final Container container) {
 		//
-		final String wrap = "wrap";
+		final String wrap = String.format("span %1$s,%2$s", 2, "wrap");
 		//
 		container.add(new JLabel("Text"));
 		container.add(tfText = new JTextField(), wrap);
@@ -48,16 +51,42 @@ public class DocumentWriter implements ActionListener {
 		//
 		container.add(new JLabel(""));
 		container.add(btnExecute = new JButton("Execute"), wrap);
-		btnExecute.addActionListener(this);
 		//
-		setWidth(250, tfText, pfOwner, pfUser);
+		container.add(new JLabel("File"));
+		container.add(tfFile = new JTextField());
+		container.add(btnCopy = new JButton("Copy"), "wrap");
+		tfFile.setEditable(false);
 		//
+		addActionListener(this, btnExecute, btnCopy);
+		//
+		final int width = 250;
+		setWidth(width - (int) btnCopy.getPreferredSize().getWidth(), tfFile);
+		setWidth(width, tfText, pfOwner, pfUser);
+		//
+	}
+
+	private static void addActionListener(final ActionListener actionListener, final AbstractButton... bs) {
+		//
+		AbstractButton b = null;
+		//
+		for (int i = 0; bs != null && i < bs.length; i++) {
+			//
+			if ((b = bs[i]) == null) {
+				continue;
+			} // skip null
+				//
+			b.addActionListener(actionListener);
+			//
+		} // for
+			//
 	}
 
 	@Override
 	public void actionPerformed(final ActionEvent evt) {
 		//
-		if (evt != null && Objects.deepEquals(evt.getSource(), btnExecute)) {
+		final Object source = evt != null ? evt.getSource() : null;
+		//
+		if (Objects.deepEquals(source, btnExecute)) {
 			//
 			final PDPage page = new PDPage();
 			final PDDocument document = new PDDocument();
@@ -76,7 +105,7 @@ public class DocumentWriter implements ActionListener {
 				contentStream.close();
 				//
 				final File file = new File("test.pdf");
-				System.out.println("file=" + file.getAbsolutePath());
+				tfFile.setText(file.getAbsolutePath());
 				//
 				// https://pdfbox.apache.org/1.8/cookbook/encryption.html
 				//
@@ -84,8 +113,6 @@ public class DocumentWriter implements ActionListener {
 				//
 				final StandardProtectionPolicy spp = new StandardProtectionPolicy(getText(pfOwner), getText(pfUser),
 						ap);
-				System.out.println(spp.getOwnerPassword());
-				System.out.println(spp.getUserPassword());
 				spp.setPreferAES(true);
 				spp.setEncryptionKeyLength(128);
 				document.protect(spp);
@@ -96,6 +123,14 @@ public class DocumentWriter implements ActionListener {
 				e.printStackTrace();
 			} finally {
 				IOUtils.closeQuietly(document);
+			}
+			//
+		} else if (Objects.deepEquals(source, btnCopy)) {
+			//
+			final Toolkit toolkit = Toolkit.getDefaultToolkit();
+			final Clipboard clipboard = toolkit != null ? toolkit.getSystemClipboard() : null;
+			if (clipboard != null) {
+				clipboard.setContents(new StringSelection(getText(tfFile)), null);
 			}
 			//
 		} // if
